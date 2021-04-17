135e1d6528a0778806340e164ea65cf31df0c47c
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-17 16:20:58 +0530

    Added login fn

--- public/app.js
+++ public/app.js
@@ -11,3 +11,21 @@ client.configure(
 		storage: window.localStorage,
 	})
 )
+
+const login = async () => {
+	try {
+		return await client.reAuthenticate()
+	} catch (err) {
+		return await client.authenticate({
+			strategy: 'local',
+			email: 'hello@feathersjs.com',
+			password: 'supersecret',
+		})
+	}
+}
+
+const main = async () => {
+	const auth = await login()
+
+	console.log('User is authenticated', auth)
+}
