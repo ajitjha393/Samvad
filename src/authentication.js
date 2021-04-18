8bdc7730b5e5bf9b059100d8db06af2c48c13eef
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-18 09:11:25 +0530

    Extended OAuth for Custom Github Auth

--- src/authentication.js
+++ src/authentication.js
@@ -1,13 +1,30 @@
-const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
-const { LocalStrategy } = require('@feathersjs/authentication-local');
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+const {
+	AuthenticationService,
+	JWTStrategy,
+} = require('@feathersjs/authentication')
+const { LocalStrategy } = require('@feathersjs/authentication-local')
+const {
+	expressOauth,
+	OAuthStrategy,
+} = require('@feathersjs/authentication-oauth')
 
-module.exports = app => {
-  const authentication = new AuthenticationService(app);
+class GithubStrategy extends OAuthStrategy {
+	async getEntityData(profile) {
+		const baseData = await super.getEntityData(profile)
 
-  authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
+		return {
+			...baseData,
+			email: profile.email,
+		}
+	}
+}
 
-  app.use('/authentication', authentication);
-  app.configure(expressOauth());
-};
+module.exports = (app) => {
+	const authentication = new AuthenticationService(app)
+
+	authentication.register('jwt', new JWTStrategy())
+	authentication.register('local', new LocalStrategy())
+
+	app.use('/authentication', authentication)
+	app.configure(expressOauth())
+}
